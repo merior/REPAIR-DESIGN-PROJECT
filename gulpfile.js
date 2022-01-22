88aaa262a053a9c6f38f448377a36bb92f0c35b8
merior nordway <meriornordway@yandex.ru>
2022-01-22 16:11:39 +0300

    добавление gulp auto prefix

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('hello', function(done){
     console.log('привет мир');
@@ -15,3 +16,11 @@ gulp.task('browser-sync', function() {
     });
     gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+exports.default = () => (
+	gulp.src('src/app.css')
+		.pipe(autoprefixer({
+			cascade: false
+		}))
+		.pipe(gulp.dest('dist'))
+);
