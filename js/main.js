7015e47847b7ec1a88eb2d953d6b9d8e6c46f5bf
merior nordway <meriornordway@yandex.ru>
2022-02-01 21:38:37 +0300

    джава скрип

--- js/main.js
+++ js/main.js
@@ -22,4 +22,31 @@ $(document).ready(function(){
     closeBtn.on('click', function (){
         modal.toggleClass('modal--visible');
     });
+
+    var mySwiper = new Swiper ('.swiper-container', {
+        loop: true
+    })
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'vertical',
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
 });
+
+
