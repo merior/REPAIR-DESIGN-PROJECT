a7f5a549b4ddc1b5f30132d8980f005f45822974
merior nordway <meriornordway@yandex.ru>
2022-02-15 22:07:03 +0300

    сжатие жс хтмл и цсс кодов

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
-const { src } = require('gulp');
+const { src, dest, series } = require('gulp');
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const minify = require('gulp-minify');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('hello', function(done){
     console.log('привет мир');
@@ -30,8 +32,28 @@ exports.default = () => (
 function buildCSS(done) {
     src('css/**/**.css')
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(dist('dest/css/'));
+        .pipe(dest('dist/css/'));
     done();
+};
+function buildJS(done) {
+    src(['js/**.js', '!js/**.min.js'])
+        .pipe(minify({
+            ext:{
+                min:'.js'
+            }
+        }))
+        .pipe(dest('dist/js/'));
+    
+    src('js/**.min.js')
+        .pipe(dest('dist/js/'));
+    done();
+};
+
+function html(done){
+    src('**.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(dest('dist/'))
+    done()
 }
 
-exports.buildCSS = buildCSS;
+exports.build = series(buildCSS, buildJS, html);
